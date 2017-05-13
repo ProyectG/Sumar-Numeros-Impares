499aab291e2b722d87be842a4440c364d0a5ebed
proyectg <proyectg.xtreme@gmail.com>
2017-05-13 03:24:16 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prueba demo = new prueba();
+            int[] dato = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int resultado = demo.sumarImpares(dato.OfType<int>().ToList());
+            Console.WriteLine("El resultado es: "+ resultado);
+            Console.ReadLine();
+        }
+
+
+        class prueba
+        {
+
+            public int sumarImpares(List<int> valores)
+            {
+                int resultado = 0;
+                for (int x = 0; x < valores.Count; x++)
+                {
+                    if ((valores[x] % 2) != 0)
+                    {
+                        resultado = resultado + valores[x];
+                    }
+                }
+                return resultado;
+            }
+        }
+    }
+}
